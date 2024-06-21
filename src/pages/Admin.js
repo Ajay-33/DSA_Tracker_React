6d524d5efad723d1c38a3ef7bd65b9bcceef9a4a
Dornala Ajay <ajaydornala1729@gmail.com>
2024-06-21 15:23:44 +0530

    Added Pages folder for clarity

---
+++ src/pages/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminPanel from '../components/AdminPanel'
+
+function Admin() {
+  return (
+    <div><AdminPanel/></div>
+  )
+}
+
+export default Admin
---
+++ src/pages/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import RolesTable from '../components/RolesTable'
+function Users() {
+  return (
+    <div>
+      <RolesTable/>
+    </div>
+  )
+}
+
+export default Users
